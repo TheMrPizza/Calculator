94a44cfa31357bdd52511180b355bb6e981f58fa
Guy <guycohen08@gmail.com>
2020-04-05 14:31:50 +0300

    Created basic operations

---
+++ Calculator/Calculator/ArithmeticUnit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class ArithmeticUnit
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Sub(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Mul(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Div(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
